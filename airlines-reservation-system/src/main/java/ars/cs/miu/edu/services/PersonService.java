9fa4476c953fcf69dad08420f292b479f71229af
Hira Kafle <16020447+hiradadiamond@users.noreply.github.com>
2021-08-07 16:55:34 -0500

    Addes interfaces in service layer

--- airlines-reservation-system/src/main/java/ars/cs/miu/edu/services/PersonService.java
+++ airlines-reservation-system/src/main/java/ars/cs/miu/edu/services/PersonService.java
@@ -1,7 +1,4 @@
 package ars.cs.miu.edu.services;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class PersonService {
+public interface PersonService {
 }
---
+++ airlines-reservation-system/src/main/java/ars/cs/miu/edu/services/PersonServiceImp.java
@@ -0,0 +1,18 @@
+package ars.cs.miu.edu.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonServiceImp {
+//
+//    View list of airports
+//    o View list of airlines flying out of an airport (search by airport three letter code)
+//    o View list of flights between a departure and destination for a date
+//    o View list of own reservations
+//    o View details of a reservation (flights, departure times, etc.)
+//    o Make a reservation (note: payload will be a list of flights)
+//    o Cancel a reservation
+//    o Confirm and purchase a reservation.
+
+
+}
